216759c722d92003b3a8107f3becb0ec1d7190ff
barbaryman <149367637+barbaryman@users.noreply.github.com>
2023-11-28 20:34:43 +0400

    task_2-2

---
+++ LR2/task _2-2.sql
@@ -0,0 +1,3 @@
+/*Выбрать информацию о наименовании и стоимости его использования для членов клуба (membercost) для всех зарегистрированных в базе объектах (facilities) клуба.*/
+USE cd;
+SELECT facility, membercost FROM facilities;
