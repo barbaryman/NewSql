8d4d58047c577ad54f1c8c5c5ccdef08a961921d
barbaryman <149367637+barbaryman@users.noreply.github.com>
2023-11-28 22:19:26 +0400

    task_2-11

---
+++ LR2/task _2-11.sql
@@ -0,0 +1,5 @@
+/*Объедините имена членов и названия объектов в обдну таблицу с одним столбцом.*/
+USE cd;
+SELECT DISTINCT CONCAT(firstname, ' ', surname) as 'Members and Facilities' FROM members WHERE firstname != 'GUEST' OR surname != 'GUEST'
+UNION
+SELECT DISTINCT facility FROM facilities;
