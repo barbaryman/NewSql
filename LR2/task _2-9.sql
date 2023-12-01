13ee565fdc534cb70e1a9f315564305264821e5a
barbaryman <149367637+barbaryman@users.noreply.github.com>
2023-12-01 13:24:39 +0400

    task_2-9

--- LR2/task _2-9.sql
+++ LR2/task _2-9.sql
@@ -1,3 +1,3 @@
 /*Выбрать всех членов клуба, зарегистрированных с сентября 2012 года.*/
 USE cd;
-SELECT surname, firstname FROM members WHERE DATE(joindate) >= '2012.09.01'; 
+SELECT surname, firstname FROM members WHERE DATE(joindate) >= '2012-09-01'; 
