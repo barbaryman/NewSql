70c49d42c1e7dcfa5fc368e54bc4f2cd68cb0e0f
barbaryman <149367637+barbaryman@users.noreply.github.com>
2023-12-01 13:20:55 +0400

    task_2-8

--- LR2/task _2-8.sql
+++ LR2/task _2-8.sql
@@ -1,3 +1,3 @@
 /*Выбрать имена объектов с их качественной стоимостью (качественной стоимостью == ‘дешевый’ если monthlymaintenance < 100 и ‘дорогой’ в противном случае.*/
 USE cd;
-SELECT facility, IF (monthlymaintenance < 100, 'дешевый', 'дорогой') AS qq FROM facilities;
+SELECT facility, IF (monthlymaintenance < 100, 'дешевый', 'дорогой') AS дор_деш FROM facilities;
