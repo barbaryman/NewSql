895e5f1ca2a0586f985acd6828a159222acc20bd
barbaryman <denymit163@gmail.com>
2023-12-03 20:18:52 +0400

    task_4-3

---
+++ LR4/task_4-3.sql
@@ -0,0 +1,4 @@
+/* Добавьте еще одно spa с такими же характеристиками как в задании 1, сгенерировав новое ID.*/
+USE cd;
+INSERT INTO facilities(facid, facility, membercost, guestcost, initialoutlay, monthlymaintenance)
+SELECT 12, facility, membercost, guestcost, initialoutlay, monthlymaintenance FROM facilities WHERE facility LIKE 'Spa' LIMIT 1;
