93a4b1bebecc90803d6d8ba637d524bb09a7118b
barbaryman <denymit163@gmail.com>
2023-12-03 19:53:31 +0400

    task_2-8

---
+++ LR2/task_2-8.sql
@@ -0,0 +1,6 @@
+/*Выбрать имена объектов с их качественной стоимостью 
+(качественной стоимостью == ‘дешевый’ если monthlymaintenance < 100 и 
+‘дорогой’ в противном случае)*/
+USE cd;
+SELECT facility, CASE WHEN monthlymaintenance < 100 THEN 'дешевый' ELSE 'дорогой' 
+END AS Качественная_стоимость FROM facilities;
