a92387529134024123077af194fdbb310211e5ce
barbaryman <denymit163@gmail.com>
2023-12-03 20:24:47 +0400

    task_4-5

---
+++ LR4/task_4-5.sql
@@ -0,0 +1,3 @@
+/* Стоимость аренды объектов 0 и 1 изменилась (член клуба == 6, гость = 30). Обновите данные в таблицах.*/
+USE cd;
+UPDATE facilities SET membercost = 6, guestcost = 30 WHERE facid = 0 OR facid = 1;
