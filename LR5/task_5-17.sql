4a88d02a3ef77a34a05acf7abd2a48ac37e3c461
barbaryman <denymit163@gmail.com>
2023-12-11 13:18:36 +0400

    изменил

--- LR5/task_5-17.sql
+++ LR5/task_5-17.sql
@@ -3,21 +3,17 @@
 Упорядочите по классификации и названию объекта.
 Примечание: используйте функцию деления на группы ntile.*/
 USE cd;
-WITH IncomeGroups AS (
-    SELECT 
-        fac.facility,
-        SUM(book.slots) AS Total_Income,
-        NTILE(3) OVER (ORDER BY SUM(book.slots)) AS Income_Group
-    FROM bookings AS book
-    INNER JOIN facilities AS fac ON book.facid = fac.facid
-    GROUP BY fac.facility
-)
-SELECT 
-    facility AS 'Название объекта',
-    CASE 
-        WHEN Income_Group = 1 THEN 'высокий'
-        WHEN Income_Group = 2 THEN 'средний'
-        ELSE 'низкий' 
-    END AS Income_Category
-FROM IncomeGroups
-ORDER BY revenue_group, facility;
+SELECT facility, 
+CASE 
+  WHEN incomegroup = 1 THEN 'Высокий доход'
+  WHEN incomegroup = 2 THEN 'Средний доход'
+  WHEN incomegroup = 3 THEN 'Низкий доход'
+END AS incomegroup
+FROM (
+  SELECT f.facility, SUM(IF(b.memid = 0, f.guestcost * b.slots, f.membercost * b.slots)) AS revenue,
+  NTILE(3) OVER (ORDER BY SUM(IF(b.memid = 0, f.guestcost * b.slots, f.membercost * b.slots))) AS incomegroup
+  FROM facilities f
+  INNER JOIN bookings b ON f.facid = b.facid
+  GROUP BY f.facility
+) as subquery
+ORDER BY incomegroup, facility;
